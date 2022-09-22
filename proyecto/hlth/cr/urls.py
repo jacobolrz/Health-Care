72152ac904ca90955d12fedd22da9251634df55e
sosk890 <112911929+sosk890@users.noreply.github.com>
2022-09-21 19:25:15 -0500

    Add files via upload

--- proyecto/hlth/cr/urls.py
+++ proyecto/hlth/cr/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from. import views
 
 urlpatterns = [
-    path('home/',views.home,name='home'),
+    path('',views.home,name='home'),
     path('register/', views.registerPage,name = 'register'),
     path('profilepage/',views.profile,name='profile'),
     path('profilepage/<str:username>/',views.profile,name='profile'),
